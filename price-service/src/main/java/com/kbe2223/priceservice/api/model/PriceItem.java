47f39bb95bfaea629643e07370954eaf77ea685e
Sameer Dml <sam.dhimal@gmail.com>
2022-11-19 11:09:20 +0100

    test completed, dummy api configured

---
+++ price-service/src/main/java/com/kbe2223/priceservice/api/model/PriceItem.java
@@ -0,0 +1,38 @@
+package com.kbe2223.priceservice.api.model;
+
+public class PriceItem {
+
+    public PriceItem(String name, String desc, double price) {
+        this.name = name;
+        this.desc = desc;
+        this.price = price;
+    }
+
+    private String name;
+    private String desc;
+    private double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
